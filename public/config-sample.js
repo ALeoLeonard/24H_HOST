1e6bcbf59c88a8f293e7a4708b0115ba364f27c8
Lauren McCarthy <laurmccarthy@gmail.com>
2017-09-26 21:17:02 -0700

    reorg

---
+++ public/config-sample.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "FIREBASE_API_KEY",
+  authDomain: "FIREBASE_AUTH_DOMAIN",
+  databaseURL: "FIREBASE_DB_URL",
+  projectId: "FIREBASE_PROJECT_ID",
+  storageBucket: "FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "FIREBASE_MESSAGING_ID",
+  fb_appId: 'FB_APP_ID'
+};
---
+++ public/data/data.js
@@ -0,0 +1,103 @@
+// Initialize Firebase
+firebase.initializeApp(config);
+firebase.auth().languageCode = 'en_EN';
+firebase.auth().signInAnonymously().catch(function(error) {
+  console.log(error);
+});
+
+var slots;
+var curId;
+
+$(document).ready(function() {
+
+  getAllSlots().then(function(s) {
+    slots = s;
+    console.log(slots)
+    for (var s in slots) {
+      if (slots[s].name) {
+        console.log(slots[s]);
+        var c = 'person';
+        if (!slots[s].identifiers || slots[s].identifiers.length == 0) c+=' needsIdentifiers';
+        //if (!slots[s].song) c+=' needsSong';
+        $('#people').append('<div id="'+slots[s].id+'" class="'+c+'">'+slots[s].time+' '+slots[s].name+'</div>');
+
+      }
+    }
+    $('.person').click(openPerson);
+  });
+
+  $('#saveIdentifiers').click(saveIdentifiers);
+  $('#showAll').click(show);
+  $('#showNeedsIdentifiers').click(function(){ show('identifiers'); });
+  //$('#showNeedsSong').click(function(){ show('song'); });
+
+});
+
+function openPerson() {
+  $('#personDetails').show();
+  var id = $(this).attr('id');
+  if (curId === id) {
+    closePerson()
+  } else {
+    curId = id;
+
+    $('#personData').html('');
+    for (var d in slots[id].data) {
+      var m = '';
+      if (slots[id].dataType === 'fb') {
+        if (slots[id].data[d].message) {
+          m += slots[id].data[d].message;
+        }
+        if (slots[id].data[d].story) {
+          m += ' ('+slots[id].data[d].story+')';
+        }
+      } else {
+        if (slots[id].data[d].text) {
+          m += slots[id].data[d].text;
+        }
+        if (slots[id].data[d].favorite_count) {
+          m += ' (fav: '+slots[id].data[d].favorite_count+')';
+        }
+      }
+      if (m) $('#personData').append('<div>'+m+'</div>');
+
+      $('#personIdentifiers').html('');
+      for (var i in slots[id].identifiers) {
+        $('#personIdentifiers').append(slots[id].identifiers[i]+'\n');
+      }
+      //$('#personSong').html(slots[id].song);
+    }
+  }
+}
+
+function closePerson() {
+  $('#personDetails').hide();
+  $('#personData').html('');
+  $('#personIdentifiers').val('');
+  curId = null;
+}
+
+function saveIdentifiers() {
+  var identifiers = $('#personIdentifiers').val();
+  slots[curId].identifiers = identifiers ? identifiers.split('\n') : null;
+  var song = $('#personSong').val();
+  slots[curId].song = song ? song : null;
+  updateSlot(curId, slots[curId]);
+  if (identifiers) {
+    $('#'+curId).removeClass('needsIdentifiers');
+    console.log('remove needsIdentifiers');
+  }
+  // console.log(song, '#'+curId);
+  // if (song) {
+  //   console.log('remove needsSong');
+  //   $('#'+curId).removeClass('needsSong');
+  // }
+  closePerson();
+}
+
+function show(arg) {
+  if (arg === 'identifiers') {
+    $('.person').hide();
+    $('.needsIdentifiers').show();
+  } else $('.person').show();
+}
